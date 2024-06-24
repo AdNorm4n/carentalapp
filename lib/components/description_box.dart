d95b8757af7dace099843cf0eccd2cb8a66e5eed
Adrian Norman <adriannorman@graduate.utm.my>
2024-06-25 00:20:43 +0800

    Delivery Page has been updated

--- lib/components/description_box.dart
+++ lib/components/description_box.dart
@@ -8,54 +8,52 @@ class DescriptionBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // textstyle
-    var myPrimaryTextStyle = 
+    var myPrimaryTextStyle =
         TextStyle(color: Theme.of(context).colorScheme.inversePrimary);
-    var mySecondaryTextStyle = 
+    var mySecondaryTextStyle =
         TextStyle(color: Theme.of(context).colorScheme.primary);
 
     return Padding(
-      padding: const EdgeInsets.only(left: 25,right: 25,bottom: 25),
+      padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
       child: Container(
         decoration: BoxDecoration(
           border: Border.all(
             color: Theme.of(context).colorScheme.secondary,
           ),
-        borderRadius: BorderRadius.circular(8),
-      ), 
-      padding: const EdgeInsets.all(25),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // booking fee
-          Column(
-            children: [
+          borderRadius: BorderRadius.circular(8),
+        ),
+        padding: const EdgeInsets.all(25),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // booking fee for 1 car/receipt
+            Column(children: [
               Text(
-                "\RM10.00",
-                 style: myPrimaryTextStyle,
-                 ),
-              Text(
-                "Booking fee", 
-                style: mySecondaryTextStyle,
-                ),
-          ] 
-          ),
-
-          // car delivery time
-          Column(
-            children: [
-              Text(
-                '20-40 mins', 
+                "\RM50.00",
                 style: myPrimaryTextStyle,
-                ),
+              ),
               Text(
-                'Car delivery time',
-                 style: mySecondaryTextStyle,
-                 ),
+                "Booking fee (1 car)",
+                style: mySecondaryTextStyle,
+              ),
+            ]),
+
+            // booking fee for more than 1 car/receipt
+            Column(
+              children: [
+                Text(
+                  '\RM100.00',
+                  style: myPrimaryTextStyle,
+                ),
+                Text(
+                  'Booking fee (multiple cars)',
+                  style: mySecondaryTextStyle,
+                ),
               ],
-           ),
-        ],
-      ), 
-    ),
-  );
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/components/receipt.dart
+++ lib/components/receipt.dart
@@ -3,37 +3,56 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class Receipt extends StatelessWidget {
-  const Receipt({super.key});
+  const Receipt({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 25, right:25, bottom: 25, top: 50),
+      padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25, top: 50),
       child: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text("Thank you for Renting!"),
-
-          const SizedBox(height: 25),
-
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Theme.of(context).colorScheme.secondary),
-              borderRadius: BorderRadius.circular(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Thank you for booking with GoRent"),
+            const SizedBox(height: 25),
+            Container(
+              decoration: BoxDecoration(
+                border:
+                    Border.all(color: Theme.of(context).colorScheme.secondary),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              padding: const EdgeInsets.all(25),
+              child: Consumer<GoRent>(
+                builder: (context, gorent, child) {
+                  final bookingDetails = gorent.getBookingDetails();
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(bookingDetails['booking']),
+                      const SizedBox(height: 10),
+                      ...bookingDetails['total_price']['items'].map<Widget>(
+                        (item) {
+                          return Text(
+                              '${item['item']}: ${item['subtotal']} (SubTotal: ${item['total']})');
+                        },
+                      ).toList(),
+                      const SizedBox(height: 10),
+                      Text(
+                          'Booking Fee: ${bookingDetails['booking_fee']}'), // Display booking fee
+                      const SizedBox(height: 10),
+                      Text(
+                          'Grand Total: ${bookingDetails['total_price']['total']}'),
+                      const SizedBox(height: 10),
+                      Text('Delivery Location: ${bookingDetails['location']}'),
+                    ],
+                  );
+                },
+              ),
             ),
-            padding: const EdgeInsets.all(25),
-            child: Consumer<GoRent>(
-              builder: (context, gorent, child) => 
-              Text(gorent.displayCartReceipt()),
-            ) ,
-          ),
-
-          const SizedBox(height: 25),
-          const Text("Estimated delivery time is around 20-40 minutes")
-        ],
+            const SizedBox(height: 25),
+          ],
+        ),
       ),
-    ),
-   );
+    );
   }
 }
